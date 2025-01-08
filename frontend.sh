741790d9ebbac5ed2d331c9ab4c29e172d32bed6
dsaniset <prem.sanisetty@gmail.com>
2025-01-08 13:22:07 +0530

    Added frontend code

---
+++ frontend.sh
@@ -0,0 +1,18 @@
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf module start nginx -y
+
+systemctl enable nginx
+systemctl start nginx
+
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+
+cd /usr/share/nginx/html
+
+unzip /tmp/fronend.zip
+
+mv nginx.conf /etc/nginx/
+
+systemctl restart nginx
