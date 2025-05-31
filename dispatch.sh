44c70287083cebac3cc7d6e1a0bc3dd927a02faf
durga.prem.sanisetty <prem.sanisetty@gmail.com>
2025-05-31 17:35:43 +0530

    a minor fix in dispatch service

--- dispatch.sh
+++ dispatch.sh
@@ -1,3 +1,4 @@
+cp dispatch.service /etc/systemd/system/dispatch.service
 dnf install golang -y
 
 useradd roboshop
@@ -10,14 +11,10 @@ cd /app
 
 unzip /tmp/dispatch.zip
 
-cd /app
 go mod init dispatch
 go get
 go build
 
-cp dispatch.service /etc/systemd/system/
-
 systemctl daemon-reload
-
 systemctl enable dispatch
-systemctl start dispatch
+systemctl restart dispatch
