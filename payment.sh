82183c6def49c12c935b355313f1176996061dbc
durga.prem.sanisetty <prem.sanisetty@gmail.com>
2025-06-07 12:49:59 +0530

    Minor fix

--- payment.sh
+++ payment.sh
@@ -1,6 +1,10 @@
 source common.sh
 app_name=payment
 
+print_heading "Setting up systemd service"
+cp $app_name.service /etc/systemd/system/$app_name.service &>>$log_file
+status_check $?
+
 print_heading "Installing Python"
 dnf install python3 gcc python3-devel -y &>>$log_file
 status_check $?
@@ -9,10 +13,6 @@ app_prerequisite
 
 print_heading "Installing application dependencies"
 pip3 install -r requirements.txt &>>$log_file
-status_check $?
-
-print_heading "Setting up systemd service"
-cp $script_path/$app_name.service /etc/systemd/system/$app_name.service &>>$log_file
 status_check $?
 
 system_restart
